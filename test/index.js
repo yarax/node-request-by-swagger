0e4bc7c3bbf4424d00df7f7c42f3b1fc6f1facf2
Jan Prieser <jaypea@users.noreply.github.com>
2018-04-09 15:13:03 +0200

    fix bug with falsy values in parameters

--- test/index.js
+++ test/index.js
@@ -28,5 +28,42 @@ describe('build options by endpoint', () => {
       done();
     });
   });
+
+  it('get with non-empty, falsy parameter', () => {
+    const path = '/pet/{petId}';
+    const endpoint = schema.paths[path].post;
+    const args = {
+      petId: 0
+    };
+    const options = {
+      method: 'get',
+      baseUrl: `http://${schema.host}${schema.basePath}`,
+      path: path,
+      args: args,
+    };
+    requestOptions = getRequestOptions(endpoint, options);
+    assert.equal(requestOptions.url, 'http://petstore.swagger.io/v2/pet/0');
+  });
+
+  it('get with empty, falsy parameter', () => {
+    const path = '/pet/{petId}';
+    const endpoint = schema.paths[path].post;
+    const args = {
+      petId: ''
+    };
+    const options = {
+      method: 'get',
+      baseUrl: `http://${schema.host}${schema.basePath}`,
+      path: path,
+      args: args,
+    };
+    try {
+      requestOptions = getRequestOptions(endpoint, options);
+    } catch (e) {
+      assert.equal(e.message, 'No required request field petId for GET /pet/{petId}')
+    }
+
+  });
+
 });
 
